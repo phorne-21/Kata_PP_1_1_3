c754fd404c999e8432f87f63dbeab8023c7b7a42
unknown <lwrow@rambler.ru>
2025-02-17 18:19:56 +0300

    add .gitignore and connection with DB in Util class

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/pp_1_1_3";
+    private static final String USER = "root";
+    private static final String PASSWORD = "qwerty12";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static void close(Connection connection) throws SQLException {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
